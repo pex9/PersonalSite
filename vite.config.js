af01873a064b1655409e4f41a397337320acd886
Davide Pellegrino s323013 <s323013@studenti.polito.it>
2024-09-29 20:50:51 +0200

    add dockerfile

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/site/', // Set this to your repository name
+  base: '/site/', // Set this to your repository name,
+  port: 3000, //set this to your desired port for local docker
 });
